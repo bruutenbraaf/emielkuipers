dbdbaea25ee3b86d988bb410cd7571cac78b7093
Bruut & Braaf <info@bruutenbraaf.nl>
2019-12-07 09:36:48 +0100

    About page

--- js/custom.js
+++ js/custom.js
@@ -18,6 +18,70 @@ var scene = new ScrollMagic.Scene({ triggerElement: "#fo" })
   .offset(-300)
   .reverse(false)
 
+var controller = new ScrollMagic.Controller();
+var steps = document.querySelectorAll('.th_img');
+for (var step of steps) {
+  var scene = new ScrollMagic.Scene({
+    triggerElement: step
+  })
+    .setClassToggle(step, 'th_img_vi')
+    .addTo(controller)
+    .offset(-250)
+    .reverse(false)
+}
+
+var controller = new ScrollMagic.Controller();
+var steps = document.querySelectorAll('.f_p p');
+for (var step of steps) {
+  var scene = new ScrollMagic.Scene({
+    triggerElement: step
+  })
+    .setClassToggle(step, 'p_s')
+    .addTo(controller)
+    .offset(-250)
+    .reverse(false)
+}
+
+
+var controller = new ScrollMagic.Controller();
+var steps = document.querySelectorAll('.cv h3');
+for (var step of steps) {
+  var scene = new ScrollMagic.Scene({
+    triggerElement: step
+  })
+    .setClassToggle(step, 'cv_hb_s')
+    .addTo(controller)
+    .offset(-250)
+    .reverse(false)
+}
+
+
+var controller = new ScrollMagic.Controller();
+var steps = document.querySelectorAll('.cv h4');
+for (var step of steps) {
+  var scene = new ScrollMagic.Scene({
+    triggerElement: step
+  })
+    .setClassToggle(step, 'cv_h_s')
+    .addTo(controller)
+    .offset(-250)
+    .reverse(false)
+}
+
+var controller = new ScrollMagic.Controller();
+var steps = document.querySelectorAll('.cv p');
+for (var step of steps) {
+  var scene = new ScrollMagic.Scene({
+    triggerElement: step
+  })
+    .setClassToggle(step, 'cv_p_s')
+    .addTo(controller)
+    .offset(-250)
+    .reverse(false)
+}
+
+
+// Delay next page  
 
 jQuery('a').click(function (e) {
   e.preventDefault();                   // prevent default anchor behavior
--- page.php
+++ page.php
@@ -1,6 +1,103 @@
 <?php
 get_header(); ?>
 
+
+<?php if (have_rows('intro')) : ?>
+    <?php while (have_rows('intro')) : the_row(); ?>
+        <section class="in_h">
+            <div class="container">
+                <div class="row">
+                    <?php if (have_rows('intro_titels')) : ?>
+                        <div class="offset-md-1 col-md-10 int">
+                            <?php while (have_rows('intro_titels')) : the_row(); ?>
+                                <span>
+                                    <p><?php the_sub_field('regel'); ?></p>
+                                </span>
+                            <?php endwhile; ?>
+                        </div>
+                    <?php endif; ?>
+                    <div class="offset-md-2 col-md-7 int__p">
+                        <?php the_sub_field('intro_tekst'); ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+    <?php endwhile; ?>
+<?php endif; ?>
+
+<?php if (have_rows('content_sections')) : ?>
+    <?php while (have_rows('content_sections')) : the_row(); ?>
+        <?php if (get_row_layout() == 'afbeelding') : ?>
+            <?php $upload_afbeelding = get_sub_field('upload_afbeelding'); ?>
+            <?php if ($upload_afbeelding) { ?>
+                <div class="container" id="im">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="f_img">
+                                <div class="th_img" style="background-image:url(<?php if ($upload_afbeelding) { ?><?php echo $upload_afbeelding['sizes']['large']; ?><?php } else { ?> <?php echo $fallback['url']; ?><?php } ?>);">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        <?php elseif (get_row_layout() == 'full_width_content') : ?>
+            <div class="container">
+                <div class="row">
+                    <div class="offset-md-2 col-md-8 f_p">
+                        <?php the_sub_field('content_wysiwyg'); ?>
+                    </div>
+                </div>
+            </div>
+        <?php elseif (get_row_layout() == 'cv') : ?>
+            <div class="container cv">
+                <div class="row">
+                    <?php if (have_rows('linke_kant')) : ?>
+                        <div class="col-md-3 offset-md-2">
+                            <?php while (have_rows('linke_kant')) : the_row(); ?>
+                                <h3><?php the_sub_field('hoofd_titel_bijv_cv_'); ?></h3>
+                                <?php if (have_rows('eigenschap')) : ?>
+                                    <?php while (have_rows('eigenschap')) : the_row(); ?>
+                                        <h4><?php the_sub_field('titel_eigenschao'); ?></h4>
+                                        <?php the_sub_field('omschrijving'); ?>
+                                    <?php endwhile; ?>
+                                <?php endif; ?>
+                                <?php $voeg_knop_toe = get_sub_field('voeg_knop_toe'); ?>
+                                <?php if ($voeg_knop_toe) { ?>
+                                    <a href="<?php echo $voeg_knop_toe['url']; ?>" target="<?php echo $voeg_knop_toe['target']; ?>"><?php echo $voeg_knop_toe['title']; ?></a>
+                                <?php } ?>
+                            <?php endwhile; ?>
+                        </div>
+                    <?php endif; ?>
+                    <?php if (have_rows('rechte_kant')) : ?>
+                        <div class="col-md-3 offset-md-1">
+                            <?php while (have_rows('rechte_kant')) : the_row(); ?>
+                                <h3><?php the_sub_field('hoofd_titel_bijv_cv_'); ?></h3>
+                                <?php if (have_rows('eigenschap')) : ?>
+                                    <?php while (have_rows('eigenschap')) : the_row(); ?>
+                                        <h4><?php the_sub_field('titel_eigenschao'); ?></h4>
+                                        <?php the_sub_field('omschrijving'); ?>
+                                    <?php endwhile; ?>
+                                <?php endif; ?>
+                                <?php $voeg_knop_toe = get_sub_field('voeg_knop_toe'); ?>
+                                <?php if ($voeg_knop_toe) { ?>
+                                    <a href="<?php echo $voeg_knop_toe['url']; ?>" target="<?php echo $voeg_knop_toe['target']; ?>"><?php echo $voeg_knop_toe['title']; ?></a>
+                                <?php } ?>
+                            <?php endwhile; ?>
+                        </div>
+                    <?php endif; ?>
+                </div>
+            </div>
+        <?php endif; ?>
+    <?php endwhile; ?>
+<?php else : ?>
+    <?php // no layouts found 
+        ?>
+<?php endif; ?>
+
+
+
+
 <div class="lxp">
     <div class="ba slidein">
     </div>
