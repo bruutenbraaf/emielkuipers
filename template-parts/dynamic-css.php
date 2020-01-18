0a228f33a12d72b18f646d4d68400d99ac51a2f5
Bruut & Braaf <info@bruutenbraaf.nl>
2020-01-18 13:34:14 +0100

    Dynamic remove

--- template-parts/dynamic-css.php
+++ template-parts/dynamic-css.php
@@ -7,15 +7,3 @@ require_once($wp_load);
 header('Content-type: text/css');
 header('Cache-control: must-revalidate');
 ?>
-
-@media (min-width: 1024px) {
-    .h__vi:first-child:before {
-        animation-delay: calc(<?php echo get_field('duur_loader', 'option'); ?>ms + 1000ms);
-    }
-    .h__vi:first-child:after {
-        animation-delay: calc(<?php echo get_field('duur_loader', 'option'); ?>ms + 1000ms);
-    }
-    .im_g {
-        animation-delay: calc(<?php echo get_field('duur_loader', 'option'); ?>ms + 1000ms);
-    }
-}
