09687eee9c7d17500a001716ad22ef68c17a97da
Bruut & Braaf <info@bruutenbraaf.nl>
2020-01-18 14:00:04 +0100

    Dynamic back

--- template-parts/dynamic-css.php
+++ template-parts/dynamic-css.php
@@ -2,6 +2,20 @@
 $absolute_path = explode('wp-content', $_SERVER['SCRIPT_FILENAME']);
 $wp_load = $absolute_path[0] . 'wp-load.php';
 require_once($wp_load);
+
+
 header('Content-type: text/css');
 header('Cache-control: must-revalidate');
 ?>
+
+@media (min-width: 1024px) {
+    .h__vi:first-child:before {
+        animation-delay: calc(<?php echo get_field('duur_loader', 'option'); ?>ms + 1000ms);
+    }
+    .h__vi:first-child:after {
+        animation-delay: calc(<?php echo get_field('duur_loader', 'option'); ?>ms + 1000ms);
+    }
+    .im_g {
+        animation-delay: calc(<?php echo get_field('duur_loader', 'option'); ?>ms + 1000ms);
+    }
+}
