b7e7dd37112054fa60ba6353e5fa846d7fad72e4
Bruut & Braaf <info@bruutenbraaf.nl>
2020-01-18 13:37:42 +0100

    Dynamic

--- template-parts/dynamic-css.php
+++ template-parts/dynamic-css.php
@@ -2,8 +2,6 @@
 $absolute_path = explode('wp-content', $_SERVER['SCRIPT_FILENAME']);
 $wp_load = $absolute_path[0] . 'wp-load.php';
 require_once($wp_load);
-
-
 header('Content-type: text/css');
 header('Cache-control: must-revalidate');
 ?>
