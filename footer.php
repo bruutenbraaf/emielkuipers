91aa6b0fa2287c85c80172688a0f0b0d0425ef09
Bruut & Braaf <info@bruutenbraaf.nl>
2020-02-08 12:46:12 +0100

    Footer

--- footer.php
+++ footer.php
@@ -2,25 +2,30 @@
 <footer id="fo">
     <div class="container">
         <div class="row">
-            <div class="offset-md-1 col-md-10 cntx">
-                <span>
-                    <p>Samenwerken met jou</p>
-                </span>
-                <span>
-                    <p>volgende project?</p>
-                </span>
-            </div>
-            <div class="offset-md-2 col-md-9 cntc">
-                <span class="s-t">
-                    <p>Emailadres</p>
-                </span>
-                <span class="s-t">
-                    <?php $e_mailadres = get_field('e-mailadres', 'option'); ?>
-                    <?php if ($e_mailadres) { ?>
-                        <a href="<?php echo $e_mailadres['url']; ?>" target="<?php echo $e_mailadres['target']; ?>"><?php echo $e_mailadres['title']; ?></a>
-                    <?php } ?>
-                </span>
-            </div>
+            <?php if (have_rows('footer', 'option')) : ?>
+                <?php while (have_rows('footer', 'option')) : the_row(); ?>
+                    <div class="offset-md-1 col-md-10 cntx">
+                        <?php if (have_rows('titels')) : ?>
+                            <?php while (have_rows('titels')) : the_row(); ?>
+                                <span>
+                                    <p><?php the_sub_field('titel'); ?></p>
+                                </span>
+                            <?php endwhile; ?>
+                        <?php endif; ?>
+                    </div>
+                    <div class="offset-md-2 col-md-9 cntc">
+                        <span class="s-t">
+                            <p>Emailadres</p>
+                        </span>
+                        <span class="s-t">
+                            <?php $e_mailadres = get_field('e-mailadres', 'option'); ?>
+                            <?php if ($e_mailadres) { ?>
+                                <a href="<?php echo $e_mailadres['url']; ?>" target="<?php echo $e_mailadres['target']; ?>"><?php echo $e_mailadres['title']; ?></a>
+                            <?php } ?>
+                        </span>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
             <?php if (have_rows('socials', 'option')) : ?>
                 <div class="col-md-12 d-flex soc">
                     <div class="ml-auto">
@@ -36,6 +41,9 @@
         </div>
     </div>
 </footer>
+
+<div class="cursor-dot-outline"></div>
+<div class="cursor-dot"></div>
 
 <div class="lxss">
     <div class="br">
