7288fe6398676b78f532fc51ed9708a72fa63d25
Manuel Schreuder <info@bruutenbraaf.nl>
2021-09-15 22:27:54 +0200

    Update frontpage

--- front-page.php
+++ front-page.php
@@ -53,6 +53,40 @@ get_header(); ?>
                 <?php endwhile; ?>
                 <?php wp_reset_postdata(); ?>
             <?php endif; ?>
+            <?php $loop = new WP_Query(array(
+                'post_type' => 'case_study',
+                'posts_per_page' => 6,
+                'offset' => 2,
+                'order' => 'DESC'
+            )); ?>
+            <?php if ($loop->have_posts()) : ?>
+                <div class="col-xl-10 offset-xl-1 pr-ov">
+                    <h3><?php _e('Overige.', 'emiel'); ?></h3>
+                </div>
+                <?php $count = 0; ?>
+                <?php while ($loop->have_posts()) : $loop->the_post(); ?>
+                    <?php $count++; ?>
+                    <div class="<?php if ($count % 2) { ?>offset-xl-1<?php } ?> col-xl-5 p-item">
+                        <a href="<?php the_permalink(); ?>" class="inner">
+                            <b><?php the_title(); ?>
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M17.7071 6.29289C18.0976 6.68342 18.0976 7.31658 17.7071 7.70711L7.70711 17.7071C7.31658 18.0976 6.68342 18.0976 6.29289 17.7071C5.90237 17.3166 5.90237 16.6834 6.29289 16.2929L16.2929 6.29289C16.6834 5.90237 17.3166 5.90237 17.7071 6.29289Z" fill="#2E2B39" />
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M6 7C6 6.44772 6.44772 6 7 6H17C17.5523 6 18 6.44772 18 7V17C18 17.5523 17.5523 18 17 18C16.4477 18 16 17.5523 16 17V8H7C6.44772 8 6 7.55228 6 7Z" fill="#2E2B39" />
+                                </svg>
+                            </b>
+                            <?php
+                            $rol_array = get_field('rol');
+                            if ($rol_array) :
+                                foreach ($rol_array as $rol_item) : ?>
+                                    <span class="r-item"><?php echo $rol_item; ?></span>
+                            <?php endforeach;
+                            endif;
+                            ?>
+                        </a>
+                    </div>
+                <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
+            <?php endif; ?>
         </div>
     </div>
 </section>
